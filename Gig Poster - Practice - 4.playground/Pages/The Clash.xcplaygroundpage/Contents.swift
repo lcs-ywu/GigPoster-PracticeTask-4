a3346088729ce7e68fa90af5ada655c257b5bdb0
Russell Gordon <github@russellgordon.ca>
2020-02-24 12:38:58 -0500

    Initial Commit

---
+++ Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+//: # Gig Poster - Practice Task 4
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+/*:
+ Each image is exactly 400 pixels wide by 600 pixels tall. 
+ 
+ Your goal is to precisely reproduce this image: 
+ 
+ ![the-clash-no-grid](the-clash-no-grid.png "The Clash")
+ ![the-clash-with-grid](the-clash-with-grid.png "The Clash")
+
+ By precisely reproducing this image, you will demonstrate your understanding of:
+ 
+ * sequence (order of statements in a program)
+ * conditionals (making decisions with ``if`` statements)
+ * iteration (use of loops to repeat statements)
+ 
+ You may use the color constants created below to obtain the correct colors.
+ 
+ Remember that you can refer to the [Canvas class documentation](https://www.russellgordon.ca/canvasgraphics/documentation/classes/canvas), as needed.
+ */
+
+// Create a new canvas
+let canvas = Canvas(width: 400, height: 600)
+
+// COLORS
+let orange = Color(hue: 21, saturation: 76, brightness: 94, alpha: 100)
+let blue = Color(hue: 201, saturation: 72, brightness: 85, alpha: 65)
+let offWhite = Color(hue: 208, saturation: 1, brightness: 88, alpha: 100)
+let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+
+// Begin your work below...
+
+
+/*:
+ ## Use Source Control
+ 
+ As this is a practice task, there is no hard deadline. However, completing this practice task – an assessment for learning – prior to attempting an assessment of learning – is strongly recommended.
+ 
+ *Be sure to commit your work multiple times as you make progress on completing this task.*
+
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gig Poster – Practice Task 4
+
+As this is a practice task, there is no hard deadline. However, completing this practice task – an assessment for learning – prior to attempting an assessment of learning – is strongly recommended.
+
+Be sure to commit your work multilple times as you make progress on completing this task.

cee12cfc779816b5da72bf8d7f4bacd3defbfeb1
James Wu <ywu@lcs.on.ca>
2020-02-26 17:15:14 -0500

    Finished all the shapes, start adding text

--- Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
@@ -32,6 +32,48 @@ let offWhite = Color(hue: 208, saturation: 1, brightness: 88, alpha: 100)
 let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 
 // Begin your work below...
+canvas.fillColor = orange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+canvas.lineColor = offWhite
+for y in stride(from: 0, through: 600, by: 50){
+    canvas.drawLine(from: Point(x:0,y:y), to: Point(x:400,y:y))
+}
+for x in stride(from: 0, through: 400, by: 50){
+    canvas.drawLine(from: Point(x: x, y: 0), to: Point(x: x, y: 600))
+}
+canvas.drawText(message: "the clash", at: Point(x: 20, y: 80), size: 48)
+canvas.drawText(message: "with the brattles", at: Point(x: 20, y: 35), size: 8)
+canvas.drawText(message: "with the brattles", at: Point(x: 20, y: 25), size: 8)
+
+canvas.rotate(by: -45)
+//canvas.drawAxes()
+//canvas.fillColor = blue
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+
+canvas.borderColor = offWhite
+for n in 0...6{
+    canvas.drawRectangle(at: Point(x: -106.6, y: 431.4), width: 12+48*n, height: 12+48*n,anchoredBy: AnchorPosition.centre, borderWidth: 12)
+}
+canvas.borderColor = blue
+//canvas.drawRectangle(at: Point(x: -155, y: 389), width: 20, height: 20)
+for n in 0...6{
+    canvas.drawRectangle(at: Point(x: -150, y: 388), width: 12+48*n, height: 12+48*n,anchoredBy: AnchorPosition.centre, borderWidth: 12)
+}
+
+//canvas.lineColor = offWhite
+//canvas.drawLine(from: Point(x: 15, y: 380), to: Point(x: 230, y: 590), lineWidth: 10)
+//canvas.drawLine(from: Point(x: 230, y: 590), to: Point(x: 445, y: 380),lineWidth: 10)
+//canvas.drawLine(from: Point(x: 445, y: 380), to: Point(x: 230, y: 160),lineWidth: 10)
+//canvas.drawLine(from: Point(x: 230, y: 160), to: Point(x: 15, y: 380),lineWidth: 10)
+//for n in stride(from: 0, through: 180, by: 30) {
+//    canvas.drawLine(from: Point(x: 15+n, y: 380), to: Point(x: 230, y: 590-n), lineWidth: 10)
+//    canvas.drawLine(from: Point(x: 230, y: 590-n), to: Point(x: 445-n, y: 380),lineWidth: 10)
+//    canvas.drawLine(from: Point(x: 445-n, y: 380), to: Point(x: 230, y: 160+n),lineWidth: 10)
+//    canvas.drawLine(from: Point(x: 230, y: 160+n), to: Point(x: 15+n, y: 380),lineWidth: 10)
+//}
+
+
 
 
 /*:
